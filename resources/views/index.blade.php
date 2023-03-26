57b595f9694f1a073141b424ee0de8607b00e14b
DiegoAbenicio <diego.abenicio1@gmail.com>
2023-03-26 17:14:15 -0300

    Cria primeira caixa

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,29 +11,54 @@
     <link rel="icon" type="image/png" href="{{ asset('img/logo.png') }}">
 </head>
 <body>
-
-    <div>
-        <img src="{{ asset('img/logo.png') }}">
-        <h2>Fixa</h2>
-        <p>
-            Fixa é um site que te conecta com profissionais qualificados para consertar, instalar ou limpar o que você precisar.
-            Você pode solicitar serviços de mecânica, pintura, eletricidade, jardinagem, marcenaria e muito mais,
-            com rapidez e segurança. Basta escolher o serviço desejado, informar o local e o horário, e pronto!
-            Um profissional irá até você e resolverá o seu problema. Fixa é a solução ideal
-            para quem precisa de serviços de qualidade sem sair de casa.
-        </p>
+    <div class="row align-items-center box">
+        <div class="col-8">
+            <div class="section texto">
+                <div class="container">
+                    <div class="full-height justify-content-center">
+                        <div class="text-left align-self-left py-5">
+                            <div class="center-wrap boxdescripition">
+                                <div class="form-row d-flex">
+                                        <div class="form-group d-flex">
+                                            <div class="align-self-start">
+                                                <img src="{{ asset('img/logo.png') }}" class="img-fluid m-6" style="max-width: 40px;">
+                                            </div>
+                                                <div style="margin-left: 10px;">
+                                                <h2>Fixa</h2>
+                                                <p class="text-justify">Fixa é um site que te conecta com profissionais qualificados para consertar, instalar ou limpar o que você precisar. Você pode solicitar serviços de mecânica, pintura, eletricidade, jardinagem, marcenaria e muito mais, com rapidez e segurança. Basta escolher o serviço desejado, informar o local e o horário, e pronto! Um profissional irá até você e resolverá o seu problema. Fixa é a solução ideal para quem precisa de serviços de qualidade sem sair de casa.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="section texto">
+                <div class="container">
+                    <div class="full-height justify-content-center">
+                        <div class="text-center align-self-center py-5">
+                            <div class="center-wrap boxdescripition">
+                                <div>
+                                    <div>
+                                        <h6>Já possui Cadastrado?</h6>
+                                        <button>Logar</button>
+                                    </div>
+                                    <div>
+                                        <h6>Não possui Cadastro?</h6>
+                                        <button>Cadastro</button>
+                                    </div>
+                                </div>
+                            </div>
+                        <div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div>
-        <div>
-            <h6>Já possui Cadastrado?</h6>
-            <button>Logar</button>
-        </div>
-        <div>
-            <h6>Não possui Cadastro?</h6>
-            <button>Cadastro</button>
-        </div>
-    </div>
 
 </body>
 </html>
