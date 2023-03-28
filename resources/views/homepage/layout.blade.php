07862e18cdb5bfc53149b3654550a4bdea3da916
DiegoAbenicio <diego.abenicio1@gmail.com>
2023-03-27 22:51:27 -0300

    Aprimora layout

--- resources/views/homepage/layout.blade.php
+++ resources/views/homepage/layout.blade.php
@@ -10,16 +10,16 @@
   <link href="{{ asset('css/wave.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ asset('css/secondpart/layout.css') }}" rel="stylesheet" type="text/css">
   <link rel="icon" type="image/png" href="{{ asset('img/logo.jpg') }}">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
-
-    <div class="svg-background">
-        <svg viewBox="0 -50 1440 320" preserveAspectRatio="none">
-            <path d="M0,96L40,117.3C80,139,160,181,240,208C320,235,400,245,480,240C560,235,640,213,720,186.7C800,160,880,128,960,122.7C1040,117,1120,139,1200,149.3C1280,160,1360,160,1400,160L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z" style="stroke: none; fill: var(--primarycolor);"></path>
-        </svg>
+    <div>
+        @yield('content')
     </div>
-    <div class="block right"></div>
     <nav class="navbar navbar-expand-lg navbar-light top-bar">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
@@ -33,15 +33,27 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#"><i class="input-icon uil uil-constructor icons "></i></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Users<i class="input-icon uil uil-user icons ml-2"></i></a>
-                </li>
+                <div class="dropdown rounded-pill dropdownstyle">
+                    <a class="nav-link active dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Users<i class="input-icon uil uil-user icons ml-2"></i>
+                    </a>
+
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="#"><i class="input-icon uil uil-user"></i>Perfil</a>
+                        <a class="dropdown-item" href="#"><i class="input-icon uil uil-question-circle"></i>Help</a>
+                        <a class="dropdown-item" href="#"><i class="input-icon uil uil-sign-out-alt"></i>Sair</a>
+                    </div>
+                </div>
             </ul>
         </div>
     </nav>
-    <div>
-        @yield('content')
-    </div>
+    <script>
+        document.querySelector('#dropdownMenuLink').addEventListener('click', function (event) {
+            event.stopPropagation();
+            document.querySelector('.dropdown-menu').classList.toggle('show');
+        });
+
+    </script>
 
 </body>
 </html>
