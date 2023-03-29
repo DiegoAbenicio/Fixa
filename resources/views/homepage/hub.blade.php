5a82ec613e7a59383aad048f450af3edbbd148bc
DiegoAbenicio <diego.abenicio1@gmail.com>
2023-03-28 22:18:12 -0300

    Começa processo de contrato

--- resources/views/homepage/hub.blade.php
+++ resources/views/homepage/hub.blade.php
@@ -1,9 +1,26 @@
 @extends('homepage.layout')
 @section('content')
-    <div class="svg-background">
-        <svg viewBox="0 -50 1440 320" preserveAspectRatio="none">
-            <path d="M0,96L40,117.3C80,139,160,181,240,208C320,235,400,245,480,240C560,235,640,213,720,186.7C800,160,880,128,960,122.7C1040,117,1120,139,1200,149.3C1280,160,1360,160,1400,160L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z" style="stroke: none; fill: var(--primarycolor);"></path>
-        </svg>
+<form>
+    <div class="form-group">
+        <label for="search">Pesquisa</label>
+        <input type="text" class="form-control" id="search">
     </div>
-    <div class="block right"></div>
+    <div class="form-group">
+        <label for="date">Data</label>
+        <input type="date" class="form-control" id="date">
+    </div>
+    <div class="form-group">
+        <label for="string1">String 1</label>
+        <input type="text" class="form-control" id="string1">
+    </div>
+    <div class="form-group">
+        <label for="string2">String 2</label>
+        <input type="text" class="form-control" id="string2">
+    </div>
+    <div class="form-group">
+        <label for="text">Texto</label>
+        <textarea class="form-control" id="text"></textarea>
+    </div>
+</form>
+
 @endsection
--- resources/views/homepage/layout.blade.php
+++ resources/views/homepage/layout.blade.php
@@ -17,9 +17,14 @@
 </head>
 
 <body>
-    <div>
-        @yield('content')
+    <div class="svg-background">
+        <svg viewBox="0 -50 1440 320" preserveAspectRatio="none">
+            <path d="M0,96L40,117.3C80,139,160,181,240,208C320,235,400,245,480,240C560,235,640,213,720,186.7C800,160,880,128,960,122.7C1040,117,1120,139,1200,149.3C1280,160,1360,160,1400,160L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z" style="stroke: none; fill: var(--primarycolor);"></path>
+        </svg>
     </div>
+    <div class="block right"></div>
+
+
     <nav class="navbar navbar-expand-lg navbar-light top-bar">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
@@ -47,6 +52,10 @@
             </ul>
         </div>
     </nav>
+    <div>
+
+        @yield('content')
+    </div>
     <script>
         document.querySelector('#dropdownMenuLink').addEventListener('click', function (event) {
             event.stopPropagation();
