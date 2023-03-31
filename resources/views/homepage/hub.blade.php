3370054be8d66065827876fc6910e7e2841cc533
DiegoAbenicio <diego.abenicio1@gmail.com>
2023-03-30 23:22:26 -0300

    Finaliza estilização do form

--- resources/views/homepage/hub.blade.php
+++ resources/views/homepage/hub.blade.php
@@ -6,31 +6,31 @@
         <div class="form-row col-xs-12 col-sm-12 col-md-12">
             <div class="form-group col-md-8">
                 <strong>Profissional</strong>
-                <input type="text" class="form-control" id="services_id">
+                <input type="text" class="form-control" id="services_id" placeholder="Insira o profissional necesserário" required>
             </div>
             <div class="form-group col-md-4">
                 <strong>Data</strong>
-                <input type="date" class="form-control" id="date">
+                <input type="date" class="form-control" id="date" required>
             </div>
         </div>
 
         <div class="form-row col-xs-12 col-sm-12 col-md-12">
             <div class="form-group col-md-7">
                 <strong>Endereço</strong>
-                <input type="text" class="form-control" id="address">
+                <input type="text" class="form-control" id="address" placeholder="Insira o endereço" required>
             </div>
             <div class="form-group col-md-5">
                 <strong>Valor</strong>
-                <input type="text" class="form-control" id="value">
+                <input type="text" class="form-control" id="value" placeholder="Valor de pagamento" required>
             </div>
         </div>
 
-        <div class="form-group">
+        <div class="form-group textplace">
             <strong>Descrição</strong>
              <textarea class="form-control textspace" name="descripition" placeholder="Entre com a descrição" required></textarea>
         </div>
 
-        <button href="submit" class="btn mt-4">Entrar</button
+        <button href="submit" class="formbtn mt-4">Entrar</button
     </form>
 </div>
 
