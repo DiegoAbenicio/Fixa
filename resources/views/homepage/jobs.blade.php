635bf399db9fb582deaa75ab1a816e17e432c0af
DiegoAbenicio <diego.abenicio1@gmail.com>
2023-04-11 20:31:03 -0300

    Cria botão de controle

--- resources/views/homepage/jobs.blade.php
+++ resources/views/homepage/jobs.blade.php
@@ -4,22 +4,32 @@
 <div class="form-row col-md-12">
 
     <div class="form-row col-md-5 contentbox">
-        <h6 class="mb-0 pb-3"><span>Entrar </span></h6>
-		<input class="checkbox" {{ session('checkbox') ? 'checked' : '' }} type="checkbox" id="reg-log" name="reg-log"/>
-        <label for="reg-log"></label>
-        <h6 class="mb-0 pb-3"><span>Entrar </span></h6>
+        <div class="form-row justify-content-center col-md-12">
+            <h6 class="mb-0 pb-3"><span>Suas Ofertas </span></h6>
+		    <input class="checkbox" {{ session('checkbox') ? 'checked' : '' }} type="checkbox" id="your-log" name="your-log"/>
+            <label for="your-log"></label>
+            <h6 class="mb-0 pb-3"><span>Ofertas aceitas </span></h6>
+        </div>
+
         <div class="form-group col-md-12 jobsbox">
 
         </div>
-    </div>
-
 
     </div>
 
     <div class="form-row col-md-5 contentbox">
-        <div class="col-md-12">Oi eu sou o botão</div>
-        <div class="form-group col-md-12 jobsbox">
+        <div class="form-row justify-content-center col-md-12">
+            <h6 class="mb-0 pb-3"><span>Ofertas disponiveis </span></h6>
+		    <input class="checkbox" {{ session('checkbox') ? 'checked' : '' }} type="checkbox" id="another-log" name="another-log"/>
+            <label for="another-log"></label>
+            <h6 class="mb-0 pb-3"><span>Ofertas aceitas </span></h6>
         </div>
+
+
+        <div class="form-group col-md-12 jobsbox">
+
+        </div>
+
     </div>
 </div>
 @else
